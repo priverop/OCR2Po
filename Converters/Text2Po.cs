ba192adc44e0907c79ba4f76331dd90ff78d892f
priverop <priverop@users.noreply.github.com>
2026-02-16 19:10:53 +0100

    feat: finish program

---
+++ Converters/Text2Po.cs
@@ -0,0 +1,56 @@
+// Copyright (c) 2026 Priverop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+using Yarhl.FileFormat;
+using Yarhl.FileSystem;
+using Yarhl.IO;
+using Yarhl.Media.Text;
+
+namespace OCR2PO.Converters
+{
+    /// <summary>
+    /// Converts a container of ImageText into a Po format.
+    /// </summary>
+    public class Text2Po :
+        IConverter<NodeContainerFormat, Po>
+    {
+        /// <summary>
+        /// Converts the NCF into a Po.
+        /// </summary>
+        /// <param name="source">NCF node.</param>
+        /// <returns>Po format.</returns>
+        public Po Convert(NodeContainerFormat source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            var po = new Po(new PoHeader("Your Project Name", "contact_email", "language"));
+
+            foreach (Node item in source.Root.Children)
+            {
+                ImageText imageText = item.GetFormatAs<ImageText>();
+
+                po.Add(new PoEntry(imageText.Text) {
+                    Context = item.Name,
+                });
+            }
+
+            return po;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,110 +1,50 @@
-﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing; 
-using Tesseract;
+﻿
+using OCR2PO.Converters;
+using Yarhl.Media.Text;
 using Yarhl.FileSystem;
 using Yarhl.IO;
 
 namespace OCR2PO
 {
-    internal class Program
+    public class Program
     {
-        static readonly string inputPath = "input";
-        static string extractedTextImageSharp = string.Empty;
-
-        static void Main()
+        static int Main(string[] args)
         {
-            Node inputFiles = NodeFactory.FromDirectory(inputPath);
-            foreach (Node file in inputFiles.Children)
-            {
-                Console.WriteLine(file.Name);
-                GetText(file.Stream);
-                Console.WriteLine("Without Any Modification of Image");
-                Console.WriteLine(extractedTextImageSharp);
 
-                GetText(ProcessImage(file.Stream));
-                Console.WriteLine("With Modification of Image");
-                Console.WriteLine(extractedTextImageSharp);
+//TODO:  outputPath
+            if (args.Length < 1) {
+                Console.WriteLine("Wrong number of arguments. Usage: OCR2PO dirPath");
+                return 1;
+        }
+
+            string inputPath = args[0];
+
+            OCR2Po(inputPath);
+            return 0;
+        }
+
+        static void OCR2Po(string inputPath)
+        {
+            var texts = new Node("ncf", new NodeContainerFormat());
+            var inputFiles = NodeFactory.FromDirectory(inputPath, "*.png");
+
+            foreach (Node file in inputFiles.Children.ToArray())
+            {
+                Console.WriteLine($"Processing {file.Name}:");
+
+                file.TransformWith<Binary2Jpeg>()
+                    .TransformWith<Jpeg2Text>();
+
+                Console.WriteLine($"Extracted text from {file.Name}.");
+
+                texts.Add(file);
             }
+
+            BinaryFormat binaryPo = texts.TransformWith<Text2Po>()
+                .TransformWith<Po2Binary>()
+                .GetFormatAs<BinaryFormat>();
             
+            binaryPo!.Stream.WriteTo("OCR.po");
         }
-
-        static void GetText(DataStream stream)
-        {  
-            stream.Position = 0;
-            var reader = new DataReader(stream);
-            byte[] buffer = reader.ReadBytes((int)stream.Length);
-
-            using (var engine = new TesseractEngine(@"./tessdata", "jpn", EngineMode.LstmOnly))
-            {
-                using var img = Pix.LoadFromMemory(buffer);
-                using var page = engine.Process(img);
-                extractedTextImageSharp = page.GetText();
-            } 
-        }
-
-        static DataStream ProcessImage(DataStream stream)
-        {
-            stream.Position = 0;
-            var processedImage = new DataStream();
-            using (Image<Rgba32> image = Image.Load<Rgba32>(stream))
-            {
-                // Invertir colores para que el texto sea oscuro sobre fondo claro
-                image.Mutate(x => x.Invert());
-                
-                // Escalar si es necesario
-                image.Mutate(x => x.Resize(image.Width * 20, image.Height * 20));
-                
-                image.SaveAsJpeg(processedImage);
-            } 
-
-            return processedImage;
-        }
-
-        // static void ImagSharpMethod(string path)
-        // {
-        //     using (Image<Rgba32> image = Image.Load<Rgba32>(path))
-        //     {
-        //         image.Mutate(x => x.Resize(image.Width * times, image.Height * times));
-        //         // Iterate through each pixel
-        //         for (int y = 0; y < image.Height; y++)
-        //         {
-        //             for (int x = 0; x < image.Width; x++)
-        //             {
-        //                 // Get the pixel color
-        //                 Rgba32 pixelColor = image[x, y];
-
-        //                 // Check if the pixel color matches the target colors
-        //                 if (IsTargetColor(pixelColor))
-        //                 {
-        //                     // Set to absolute white
-        //                     image[x, y] = Rgba32.ParseHex("#fff");
-        //                 }
-        //             }
-        //         }
-                 
-        //         // Save the modified image
-        //         image.Save(modifiedImagePath);
-        //     } 
-        // }
-
-        // static bool IsTargetColor(Rgba32 color)
-        // { 
-
-        //     // Blue and light blue range
-        //     if (color.B > 128 && color.R < 128 && color.G < 128)
-        //         return true;
-
-        //     // Grey range
-        //     if (color.R > 128 && color.R < 200 && color.G > 128 && color.G < 200 && color.B > 128 && color.B < 200) return true;
-
-        //     // Yellow range
-        //     if (color.R > 200 && color.G > 200 && color.B < 128)
-        //         return true;
-
-        //     // Add more color checks as needed
-
-        //     return false;
-        // }
     }
 }
